065a7ea0496b3f948b3df69742a367ca33210398
Rajvaibhav Rahane <rrahane@amazon.com>
2025-02-25 16:35:42 -0800

    Bug: Repo maintenance (#14)
    * Add Initial Repo Maintenance Files.
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * add github templates, workflows, codeowners
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * Add RELEASING.md and .codecov.yml
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * Add Test CI
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * fix: add pytest to test requirements
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * bug: move run-tests.yml to base workflows folder
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * nit: resolve lint errors
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * nit: resolve mypy errors
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * nit: resolve rename errors in run-tests.py
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * add test placeholder
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * add test placeholder
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * Change test config to setup.cfg
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * nit: delete renamed folders, fix placeholder test
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * Add Test CI
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * fix: add pytest to test requirements
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * bug: move run-tests.yml to base workflows folder
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * nit: resolve lint errors
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * nit: resolve mypy errors
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * nit: resolve rename errors in run-tests.py
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * add test placeholder
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * add test placeholder
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * Repo maintenance (#10)
    
    * Add Initial Repo Maintenance Files.
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * add github templates, workflows, codeowners
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * Add RELEASING.md and .codecov.yml
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * Add Test CI
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * fix: add pytest to test requirements
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    ---------
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * Change test config to setup.cfg
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    * nit: delete renamed folders, fix placeholder test
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>
    
    ---------
    
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>

--- test_remote_vector_index_builder/test_placeholder.py
+++ test_remote_vector_index_builder/test_placeholder.py
@@ -4,3 +4,7 @@
 # The OpenSearch Contributors require contributions made to
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
+
+
+def test_placeholder():
+    assert 1 + 1 == 2
